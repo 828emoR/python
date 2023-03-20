9567b8ae1f99678bf14704613c9650cdbd03b587
828emoR <828emoR@gmail.com>
2023-03-20 09:16:40 +0900

    0320

---
+++ hellow.py
@@ -0,0 +1,19 @@
+print(2+3)
+print("kim")
+print(2+3, "kim")
+#a = 3+4
+a = "dd"
+age =26
+print(a)
+# print(type(a))
+
+i_name="kim"
+f_name="juHwan"
+print("kim's age is",  age)
+print("his full name is", i_name, f_name, "and his age is", age)
+print("오늘 배운것 = 변수선언+str, int 타입 출력")
+
+if a : str(
+        print("a's type is string")
+)
+
