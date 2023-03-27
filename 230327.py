3d8e54a40aa2b6328b6b191ba08199ff9cf8ac77
828emoR <https://github.com/828emoR/CSS-HTML.git>
2023-03-27 15:47:44 +0900

    230327오후

--- 230327.py
+++ 230327.py
@@ -56,4 +56,41 @@ a2 = int(input("2번 수 입력"))
 if a2 == a1 :print(f"{a1}와{a2}는 같습니다.")
 elif a2 < a1: print(f"{a1}은{a2}보다 큽니다.")
 else: print(f"{a2}는{a1}보다 큽니다.")
+
+
+a = int(input("1번 입력 : "))
+b = int(input("2번 입력 : "))
+c = int(input("3번 입력 : "))
+
+max = a
+if max < b : 
+    max = b
+if max < c:
+    max = c
+print(f"{max}가 가장 큰 수 입니다.")
+
+# if a < b : 
+#     if b < c :
+#         print("c")
+#     else : 
+#         print("B")
+# else : 
+#     if a > c :
+#         print("A")
+#     else : 
+#         print("C")
 '''
+# a = [0,1,2,3,4,5,6,7,8,9]
+
+# print(a[3])
+# print(a[0:4])
+# print(a[3:6])
+# print(a[7:])
+# print(a[:3])
+
+a = int(input("입력"))
+
+if a > 100:
+    print(f"{a}는 100보다 큽니다.")
+if 100 > a > 50 : print(f"{a}는 100과 50사이 수 입니다.")
+elif 50 > a : print(f"{a}는 50 이하의 수 입니다.")
